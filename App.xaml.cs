cdaa71c535bff66d3a6a919113e1a20ce694734e
felipeoliveiradn2 <felipeoliveiradn2@gmail.com>
2025-05-30 13:02:43 -0300

    atividade recuperação 13 CursoDs2

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,45 @@
-﻿namespace recAgenda13Ds2
+﻿using recAgenda13Ds2.Models;
+
+namespace recAgenda13Ds2
 {
     public partial class App : Application
     {
+        //array com lista de quartos
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suite super luxo",
+                ValorDiariaAdulto= 110.00,
+                ValorDiariaCrianca = 55.00
+
+            },
+            new Quarto()
+            {
+                Descricao = "Suite luxo",
+                ValorDiariaAdulto= 80.00,
+                ValorDiariaCrianca = 40.00
+
+            },
+            new Quarto()
+            {
+                Descricao = "Suite Single",
+                ValorDiariaAdulto= 50.00,
+                ValorDiariaCrianca = 25.00
+
+            },
+            new Quarto()
+            {
+                Descricao = "Suite da crise",
+                ValorDiariaAdulto= 25.00,
+                ValorDiariaCrianca = 12.50
+            }
+        };
         public App()
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
         }
         protected override Window CreateWindow(IActivationState? activationState)
         {
